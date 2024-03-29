c10949c576d13d397bbd545c422ee3188d06a805
HareemSaad <hareems20014@outlook.com>
2024-03-29 15:48:56 +0500

    Loading an existing keypair from an .env file

--- Solana/index.ts
+++ Solana/index.ts
@@ -1,6 +1,6 @@
-import { generating_keypair } from "./modules/addresses";
+import { load_public_key_from_existing_private_key } from "./modules/addresses";
 
 
 (async function () {
-    generating_keypair();
+    load_public_key_from_existing_private_key();
 })();
--- Solana/modules/addresses.ts
+++ Solana/modules/addresses.ts
@@ -1,8 +1,21 @@
+import { getKeypairFromEnvironment } from "@solana-developers/helpers";
 import { Keypair } from "@solana/web3.js";
+import "dotenv/config";
 
 export function generating_keypair() {
 
     const keypair = Keypair.generate();
+    
+    console.log(`The public key is: `, keypair.publicKey.toBase58());
+    console.log(`The secret key is: `, keypair.secretKey);
+    
+}
+
+export function load_public_key_from_existing_private_key() {
+
+    // put it in .env as VAR=[56,79,...]
+
+    const keypair = getKeypairFromEnvironment("SECRET_KEY");
     
     console.log(`The public key is: `, keypair.publicKey.toBase58());
     console.log(`The secret key is: `, keypair.secretKey);
