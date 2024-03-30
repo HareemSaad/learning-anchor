35078f8323fa34dfe1069092578aede843be3b34
HareemSaad <hareems20014@outlook.com>
2024-03-30 20:03:27 +0500

    associate token account

--- Solana/modules/fungible_token.ts
+++ Solana/modules/fungible_token.ts
@@ -2,10 +2,13 @@ import {
   MINT_SIZE,
   TOKEN_PROGRAM_ID,
   createAccount,
+  createAssociatedTokenAccount,
+  createAssociatedTokenAccountInstruction,
   createInitializeAccountInstruction,
   createInitializeMintInstruction,
   createMint,
   getAccountLenForMint,
+  getAssociatedTokenAddress,
   getMinimumBalanceForRentExemptMint,
   getMint,
 } from "@solana/spl-token";
@@ -32,10 +35,10 @@ const sender_key_pair = load_key(sender_string);
 // const freezer_key_pair = load_key(sender_string);
 
 // To create a new SPL-Token you first have to create a Token Mint. A Token Mint is the account that holds data about a specific token. such as the current supply of tokens, the addresses of the mint and freeze authorities, and the decimal precision of the token. Things like metadata
-export async function creating_a_token_mint() {
+export async function creating_a_token_mint(): Promise<PublicKey> {
   const decimal = 6;
 
-  const tokenMintPubKey = await createMint(
+  const tokenMintPubKey = await createMint( // lies on the Ed25519 curve; is a program
     connection,
     sender_key_pair, //payer
     sender, // the account which is authorized to do the actual minting of tokens from the token mint.
@@ -44,21 +47,38 @@ export async function creating_a_token_mint() {
   );
 
   console.log("Mint account: ", tokenMintPubKey.toString());
+
+  return tokenMintPubKey;
 }
 
 // Token Account to hold the newly issued tokens.
 export async function creating_a_token_account() {
-  const key_pair = await Keypair.generate();
+  const key_pair = await Keypair.generate(); // lies on the Ed25519 curve; is a program
 
   const account = await createAccount(
     connection,
     sender_key_pair, //payer
-    sender, // the token mint that the new token account is associated with
+    await creating_a_token_mint(), // the token mint that the new token account is associated with
     sender, // the account of the owner of the new token account
     key_pair
   );
 
   console.log("\nToken account: ", account.toString());
+}
+
+// An Associated Token Account is a Token Account where the address of the Token Account is derived using an owner's public key and a token mint.
+// If not for associated token account, a user may own many token accounts belonging to the same mint leading to confusion as to where to send tokens to.
+// Associated token account allows a user to send tokens to another user if the recipient doesn't yet have the token account for that token mint.
+export async function creating_an_associate_token_account() {
+
+  const associatedTokenAccount = await createAssociatedTokenAccount(
+    connection,
+    sender_key_pair, //payer
+    await creating_a_token_mint(), // the token mint that the new token account is associated with
+    sender, // the account of the owner of the new token account
+  );
+
+  console.log("\nAssociated token account: ", associatedTokenAccount.toString());
 }
 
 (async function () {})();
@@ -125,3 +145,24 @@ async function buildCreateTokenAccountTransaction(
 
   return transaction;
 }
+
+// Under the hood, createAssociatedTokenAccount is doing two things:
+// Using getAssociatedTokenAddress to derive the associated token account address from the mint and owner
+// Building a transaction using instructions from createAssociatedTokenAccountInstruction
+async function buildCreateAssociatedTokenAccountTransaction(
+    payer: PublicKey,
+    mint: PublicKey
+  ): Promise<Transaction> {
+    const associatedTokenAddress = await getAssociatedTokenAddress(mint, payer, false);
+  
+    const transaction = new Transaction().add(
+      createAssociatedTokenAccountInstruction(
+        payer,
+        associatedTokenAddress,
+        payer,
+        mint
+      )
+    )
+  
+    return transaction
+  }
