80be0880eb6850389bce60b96e827abf0e6eb3f1
HareemSaad <hareems20014@outlook.com>
2024-05-12 19:32:31 +0500

    only owner functionality + tests

--- Anchor/voting/programs/voting/src/lib.rs
+++ Anchor/voting/programs/voting/src/lib.rs
@@ -2,14 +2,38 @@ use anchor_lang::prelude::*;
 
 declare_id!("5Zbhwu3r2rBFqopK8S8oPrwcC27Ma8b9ytdaCKJNcjiX");
 
+const OWNER: &str = "8jjpdyVroFJfsEi4E4XsDTH5VzcponDwWYCYFVTcPJeF";
+
 #[program]
 pub mod voting {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    #[access_control(check(&ctx))]
+    pub fn init_candidate(ctx: Context<InitializeCandidate>) -> Result<()> {
+        msg!("Init Candidate! {:?}", ctx.accounts.payer.key());
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+
+pub struct InitializeCandidate<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+}
+
+fn check(ctx: &Context<InitializeCandidate>) -> Result<()> {
+    // Check if signer === owner
+    require_keys_eq!(
+        ctx.accounts.payer.key(),
+        OWNER.parse::<Pubkey>().unwrap(),
+        OnlyOwnerError::NotOwner
+    );
+    Ok(())
+}
+
+#[error_code]
+pub enum OnlyOwnerError {
+    #[msg("Only owner can call this function!")]
+    NotOwner,
+}
--- Anchor/voting/tests/voting.ts
+++ Anchor/voting/tests/voting.ts
@@ -1,16 +1,35 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { Voting } from "../target/types/voting";
+import { expect } from "chai";
 
 describe("voting", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.Voting as Program<Voting>;
 
+  let nonOwner = anchor.web3.Keypair.generate();
+
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const tx = await program.methods.initCandidate().rpc();
     console.log("Your transaction signature", tx);
+  });
+
+  it("fails to initialize candidate by non-owner", async () => {
+
+    try {
+      await program.methods
+        .initCandidate()
+        .accounts({
+          // payer: program.provider.publicKey
+          payer: nonOwner.publicKey,
+        })
+        .rpc();
+    } catch (error) {
+      expect(error.message).to.equal(`Signature verification failed.\nMissing signature for public key [\`${nonOwner.publicKey}\`].`);
+    }
   });
 });
