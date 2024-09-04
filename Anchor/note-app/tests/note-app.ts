438464d6453d69bf7c65cc2c4ee0af13afbfd4c9
HareemSaad <hareems20014@outlook.com>
2024-09-04 16:14:35 +0500

    assert count

--- Anchor/note-app/tests/note-app.ts
+++ Anchor/note-app/tests/note-app.ts
@@ -54,7 +54,9 @@ describe("note-app", () => {
       program.programId
     );
 
-    const counter = await program.account.count.fetch(counterAccount);
+    let counter = await program.account.count.fetch(counterAccount);
+
+    assert.ok(counter.count.toNumber() === 1);
     
     await program.methods
     .create("Hello World!")
@@ -70,8 +72,10 @@ describe("note-app", () => {
     );
 
     const account = await program.account.note.fetch(noteAccount);
+    counter = await program.account.count.fetch(counterAccount);
 
     assert.ok(account.note === "Hello World!");
+    assert.ok(counter.count.toNumber() === 2);
   });
 
   it("Updates a note", async () => {
@@ -93,4 +97,88 @@ describe("note-app", () => {
 
     assert.ok(account.note === "Hello World! Updated");
   });
+
+  it("Creates a note for multiple users", async () => {
+    const user1 = await createUser();
+    
+    await program.methods
+    .initialize()
+    .accounts({
+      payer: user1.publicKey
+    })
+    .signers([user1])
+    .rpc();
+
+    const [counterAccount] = PublicKey.findProgramAddressSync(
+      [user1.publicKey.toBuffer()],
+      program.programId
+    );
+
+    const counter = await program.account.count.fetch(counterAccount);
+
+    await program.methods
+    .create("Hello World!")
+    .accounts({
+      counter: counterAccount,
+      payer: user1.publicKey
+    })
+    .signers([user1])
+    .rpc();
+
+    const [noteAccount] = PublicKey.findProgramAddressSync(
+      [Buffer.from("note"), user1.publicKey.toBuffer(), new anchor.BN(counter.count).toArrayLike(Buffer, "le", 8)],
+      program.programId
+    );
+
+    const account = await program.account.note.fetch(noteAccount);
+
+    assert.ok(account.note === "Hello World!");
+
+    const user2 = await createUser();
+
+    await program.methods
+    .initialize()
+    .accounts({
+      payer: user2.publicKey
+    })
+    .signers([user2])
+    .rpc();
+
+    const [counterAccount2] = PublicKey.findProgramAddressSync(
+      [user2.publicKey.toBuffer()],
+      program.programId
+    );
+
+    const counter2 = await program.account.count.fetch(counterAccount2);
+
+    await program.methods
+    .create("Hello World2!")
+    .accounts({
+      counter: counterAccount2,
+      payer: user2.publicKey
+    })
+    .signers([user2])
+    .rpc();
+
+    const [noteAccount2] = PublicKey.findProgramAddressSync(
+      [Buffer.from("note"), user2.publicKey.toBuffer(), new anchor.BN(counter2.count).toArrayLike(Buffer, "le", 8)],
+      program.programId
+    );
+
+    const account2 = await program.account.note.fetch(noteAccount2);
+
+    assert.ok(account2.note === "Hello World2!");
+  });
+
+  // create a function that will create a new user and give it lamports
+  async function createUser(): Promise<anchor.web3.Keypair> {
+    const  user = anchor.web3.Keypair.generate()
+
+    //give lamports to user1
+    const lamports = 1_000_000_000_000;
+    const signature = await provider.connection.requestAirdrop(user.publicKey, lamports);
+    await provider.connection.confirmTransaction(signature);
+
+    return user;
+  }
 });
