dcd4b548efa0770a82390b8b37a3de9f360ca04e
HareemSaad <hareems20014@outlook.com>
2024-09-04 11:37:46 +0500

    feat: Add data validation to Note App initialization. PDA is generated through keypair

--- Anchor/note-app/programs/note-app/src/lib.rs
+++ Anchor/note-app/programs/note-app/src/lib.rs
@@ -6,11 +6,39 @@ declare_id!("HMopToKhdRbGy81PFuxynZisN3YEV3Eq7jx6ftJyMcCk");
 pub mod note_app {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(ctx: Context<Initialize>, data: String) -> Result<()> {
+        //  check if data is less than 100 bytes
+        if data.len() > 100 {
+            return Err(ErrorCode::DataTooLong.into());
+        }
         msg!("Initializing Note App program");
+        let note = &mut ctx.accounts.note;
+        note.note = data;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize<'info> {
+    // The note (data) account to initialize.
+    #[account(init, payer = payer, space = 8 + Note::INIT_SPACE)]
+    pub note: Account<'info, Note>,
+    // The user account to initialize the note account.
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    // The system program account.
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct Note {
+    #[max_len(100)]
+    pub note: String,
+}
+
+#[error_code]
+pub enum ErrorCode {
+    #[msg("Data too long")]
+    DataTooLong,
+}
--- Anchor/note-app/tests/note-app.ts
+++ Anchor/note-app/tests/note-app.ts
@@ -1,17 +1,52 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { NoteApp } from "../target/types/note_app";
+import { assert, expect } from "chai";
 
+// run on localhost `solana config set --url localhost`
 describe("note-app", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.NoteApp as Program<NoteApp>;
+  const wallet = anchor.workspace.NoteApp.provider.wallet;
 
   it("Is initialized!", async () => {
+    // Generate a new keypair for the note account
+    const noteAccount = anchor.web3.Keypair.generate();
+    // console.log(wallet.publicKey.toBase58(), program.provider.publicKey.toBase58(), program.programId.toBase58(), noteAccount.publicKey.toBase58());
+
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
-    
+    const tx = await program.methods
+      .initialize("Hello World!")
+      .accounts({
+        note: noteAccount.publicKey,
+      })
+      .signers([noteAccount])
+      .rpc();
+    // console.log("Your transaction signature", tx);
+    // console.log("NoteApp program ID", program.programId.toBase58());
+
+    const account = await program.account.note.fetch(noteAccount.publicKey);
+
+    assert.ok(account.note === "Hello World!");
+  });
+
+  it("Does not initialize if note exceeds 100 bytes", async () => {
+    // Generate a new keypair for the note account
+    const noteAccount = anchor.web3.Keypair.generate();
+
+    try {
+      await program.methods
+        .initialize("lorem ipsum dolor sit amet, consectetur adipiscing elit. sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.")
+        .accounts({
+          note: noteAccount.publicKey,
+        })
+        .signers([noteAccount])
+        .rpc();
+    } catch (error) {
+      expect(error.message).to.include("Data too long");
+    }
   });
 });
